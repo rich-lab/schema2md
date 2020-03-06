2aa9754257aaa98bc4f8dac398a8fede078aa8d6
ULIVZ <472590061@qq.com>
2020-03-07 06:31:36 +0800

    test: improvement

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,4 @@
-const { transform } = require('../lib')
+const { transform, batchTransform } = require('../lib')
 
 it('simple', async () => {
   const ret = await transform({
@@ -30,4 +30,41 @@ it('merge md', async () => {
   })
 
   expect(ret).toMatchSnapshot()
+})
+
+it('batchTransform', async () => {
+  const configs = [
+    {
+      schemaPath: 'fixtures/simple.json',
+      outputPath: 'docs/config/README.md',
+    },
+    {
+      locale: 'zh-CN',
+      schemaPath: 'fixtures/doc.json',
+      outputPath: 'docs/config/README.md',
+    }
+  ]
+
+  const defaultConfog = {
+    cwd: __dirname,
+    write: false,
+  }
+
+  const ret = await batchTransform({
+    ...defaultConfog,
+    configs,
+  })
+
+  const ret1 = await transform({
+    ...defaultConfog,
+    ...configs[0]
+  })
+
+  const ret2 = await transform({
+    ...defaultConfog,
+    ...configs[1]
+  })
+
+  expect(ret[0]).toEqual(ret1)
+  expect(ret[1]).toEqual(ret2)
 })
